295e30b9979db567980dadb7afa83eba5ac99f94
Hans Lemkes <hanslemkes@gmail.com>
2024-11-28 22:36:38 +0100

    add values and create namespaces in de install values

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+echo "install ArgoCd"
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+echo "create different namespaces"
+kubectl create namespace test-tst
+kubectl create namespace test-acc
+kubectl create namespace test-prd
